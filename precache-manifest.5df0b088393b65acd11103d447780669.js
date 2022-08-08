269f6807b3f7d9fc50d67b9ab1fbd6be8489f67c
Gurkirat Singh <kiratriar1313@gmail.com>
2022-08-08 07:04:52 +0530

    latest commit

---
+++ precache-manifest.5df0b088393b65acd11103d447780669.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d67f3e8dca17d767ff8",
+    "url": "/Text-Analyzer/static/css/main.da4317bb.chunk.css"
+  },
+  {
+    "revision": "6d67f3e8dca17d767ff8",
+    "url": "/Text-Analyzer/static/js/main.b8c7624d.chunk.js"
+  },
+  {
+    "revision": "a17cb65e980f77220e8f",
+    "url": "/Text-Analyzer/static/js/runtime~main.e5598683.js"
+  },
+  {
+    "revision": "a02664e0b39859221ec2",
+    "url": "/Text-Analyzer/static/js/2.d011160c.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/Text-Analyzer/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "970e47a67d17bb8aa71d80c74c33ac31",
+    "url": "/Text-Analyzer/index.html"
+  }
+];
---
+++ src/components/About.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+export default function About(props) {
+
+    // const [myStyle, setMyStyle] = useState({
+    //     color: 'black',
+    //     backgroundColor: 'white'
+    // }) 
+    let myStyle = {
+        color: props.mode ==='dark'?'white':'#042743',
+        backgroundColor: props.mode ==='dark'?'rgb(36 74 104)':'white', 
+    }
+    
+    return (
+        <div className="container">
+            <h1 className="my-3" style={{color: props.mode ==='dark'?'white':'#042743'}}>About Us</h1>
+            <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" style={myStyle} data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                    <strong>About Text Analyzer</strong>
+                    </button>
+                    </h2>
+                    <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    Text Analyzer is react based website which is fast and goes vroom-vroom and gives you a way to analyze your text quickly and efficiently.
+                     Be it word count or character count. The feature of this website is that you can manipulate text however, you want! You are a nerd if you are reading this.
+
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    <strong>About Nothing!</strong>
+                    </button>
+                    </h2>
+                    <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    
+                    <div className="accordion-body" style={myStyle}>
+                    Nothing to see here 😏
+                    </div>
+                    </div>
+                </div>
+                <div className="accordion-item">
+                    <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    <strong>About Developer </strong>
+                    </button>
+                    </h2>
+                    <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body" style={myStyle}>
+                    Yo! This is Gurkirat Singh
+
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
